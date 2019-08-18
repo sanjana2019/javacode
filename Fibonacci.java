ebfe21ee021094521fabc6d8a6c3917ebcaacd37
sanjana2019 <47409877+sanjana2019@users.noreply.github.com>
2019-08-18 21:54:08 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Fibonacci
+{
+	public static void main (String args[])
+	{
+		int n1=0,n2=1,n3,i,number;
+		Scanner sc = new Scanner (System.in);
+		System.out.print("Enter the no. of elements");
+		number=sc.nextInt();
+		System.out.print( +n1 +n2);
+		     
+		for(i=2;i<number;++i)   
+ 		{    
+  			n3=n1+n2;    
+  			System.out.print(+n3);    
+ 			 n1=n2;    
+ 			 n2=n3;    
+ 		}  
+	}
+}
